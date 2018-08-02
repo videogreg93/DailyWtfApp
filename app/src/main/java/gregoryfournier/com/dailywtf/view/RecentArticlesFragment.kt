bfd34481311ac4d63352d780588de8ec610876e6
Gregory Fournier <gregory.fournier@logient.com>
2018-08-02 10:48:09 -0400

    Init

---
+++ app/src/main/java/gregoryfournier/com/dailywtf/view/RecentArticlesFragment.kt
@@ -0,0 +1,22 @@
+package gregoryfournier.com.dailywtf.view
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import gregoryfournier.com.dailywtf.R
+
+
+class RecentArticlesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recent_articles, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/gregoryfournier/com/dailywtf/view/utils/ViewUtils.kt
@@ -0,0 +1,71 @@
+package gregoryfournier.com.dailywtf.view.utils
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.view.View
+import gregoryfournier.com.dailywtf.R
+
+
+fun View.showSnackBar(message: String, duration: Int): Snackbar {
+    val snackbar = Snackbar.make(this, message, duration)
+    snackbar.show()
+    return snackbar
+}
+
+fun View.showSnackBarButton(message: String, duration: Int, actionTitleResId: Int, action: View.OnClickListener): Snackbar {
+    val snackbar = Snackbar.make(this, message, duration)
+            .setAction(actionTitleResId, action)
+
+    snackbar.show()
+    return snackbar
+}
+
+fun View.showSnackBarButton(message: String, duration: Int, actionTitle: String, action: View.OnClickListener): Snackbar {
+    val snackbar = Snackbar.make(this, message, duration)
+            .setAction(actionTitle, action)
+    snackbar.show()
+    return snackbar
+}
+
+//TODO put in LLUIFOUNDATION when created
+object ViewUtils {
+    /**
+     * Replace the current fragment without arguments
+     *
+     * @param fragmentToDisplay new fragment
+     * @param addToBackStack    true if the new fragment should be added to the backstack
+     */
+    fun displayFragmentWithoutArgs(activity: FragmentActivity, fragmentToDisplay: Fragment, addToBackStack: Boolean) {
+        val fragment = activity.supportFragmentManager.findFragmentByTag(fragmentToDisplay.javaClass.name)
+        if (fragment != null) {
+            activity.supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.content, fragment, fragmentToDisplay.javaClass.name).commit()
+        } else {
+            val transaction = activity.supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.content, fragmentToDisplay, fragmentToDisplay.javaClass.name)
+            if (addToBackStack) {
+                transaction.addToBackStack(fragmentToDisplay.javaClass.name)
+            }
+            transaction.commit()
+        }
+    }
+
+
+    /**
+     * Replace the current fragment
+     *
+     * @param fragmentToDisplay new fragment
+     * @param addToBackStack    true if the new fragment should be added to the backstack
+     * @param args              fragment arguments
+     */
+    fun displayFragmentWithArgs(activity: FragmentActivity, fragmentToDisplay: Fragment, addToBackStack: Boolean, args: Bundle?) {
+        fragmentToDisplay.arguments = args
+        displayFragmentWithoutArgs(activity, fragmentToDisplay, addToBackStack)
+    }
+
+}
